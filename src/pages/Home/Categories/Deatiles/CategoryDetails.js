0608ccbb0ea8f04ddb06c1a09574e5121333aca7
maksudalam <maksud.cdz@gmail.com>
2022-11-25 12:49:41 +0600

    Modal

--- src/pages/Home/Categories/Deatiles/CategoryDetails.js
+++ src/pages/Home/Categories/Deatiles/CategoryDetails.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import ProductDetails from './ProductDetails';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import BookModal from "./BookModal/BookModal";
+import ProductDetails from "./ProductDetails";
 
 const CategoryDetails = () => {
-    const category=useLoaderData()
-    return (
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-items-center gap-4 bg-[#131313]'>
-            {
-                category.products.map(product=><ProductDetails
-                    product={product}
-                ></ProductDetails>)
-            }
-        </div>
-    );
+  const category = useLoaderData();
+  const [product,setProduct]=useState(null)
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-items-center gap-4 bg-[#131313]">
+      {category.products.map((product) => (
+        <ProductDetails 
+        product={product}
+        setProduct={setProduct}
+        ></ProductDetails>
+      ))}
+      <div>
+        <BookModal
+        product={product}
+        ></BookModal>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryDetails;
--- src/pages/Home/Categories/Deatiles/ProductDetails.js
+++ src/pages/Home/Categories/Deatiles/ProductDetails.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React from "react";
 
-const ProductDetails = ({product}) => {
-    const {img,name,location,resaleprice,originalprice,yearofuse,sellerName,condition}=product
+const ProductDetails = ({ product,setProduct }) => {
+  const {
+    img,
+    name,
+    location,
+    resaleprice,
+    originalprice,
+    yearofuse,
+    sellerName,
+    condition,
+  } = product;
 
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
-        <figure><img src={img} alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>Location: {location}</p>
-          <p>Resale-Price: {resaleprice}</p>
-          <p>Orginal-Price{originalprice}</p>
-          <p>Use: {yearofuse}</p>
-          <p>Seller-Name:{sellerName}</p>
-          <p>{condition}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
-          </div>
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <figure>
+        <img src={img} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>Location: {location}</p>
+        <p>Resale-Price: {resaleprice}</p>
+        <p>Orginal-Price{originalprice}</p>
+        <p>Use: {yearofuse}</p>
+        <p>Seller-Name:{sellerName}</p>
+        <p>{condition}</p>
+        <div className="card-actions justify-center">
+          <label 
+          onClick={()=>setProduct(product)}
+          htmlFor="my-modal-3" 
+          className="btn btn-primary">
+            Book Now
+          </label>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ProductDetails;
